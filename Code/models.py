be58c74a0559c10a74bcfefa45e28cb8c11b7c69
Skylar Wurster <swwurster@gmail.com>
2021-11-01 13:13:32 -0400

    Update models.py

--- Code/models.py
+++ Code/models.py
@@ -62,78 +62,63 @@ class PositionalEncoding(nn.Module):
             locations[..., 5::6] = torch.cos(locations[..., 5::6])
         return locations
 
-def init_weights_normal(m):
-    if type(m) == nn.Linear:
-        if hasattr(m, 'weight'):
-            nn.init.kaiming_normal_(m.weight, a=0.0, nonlinearity='relu', mode='fan_in')
-
-
-def init_weights_selu(m):
-    if type(m) == nn.Linear:
-        if hasattr(m, 'weight'):
-            num_input = m.weight.size(-1)
-            nn.init.normal_(m.weight, std=1 / math.sqrt(num_input))
-
-
-def init_weights_elu(m):
-    if type(m) == nn.Linear:
-        if hasattr(m, 'weight'):
-            num_input = m.weight.size(-1)
-            nn.init.normal_(m.weight, std=math.sqrt(1.5505188080679277) / math.sqrt(num_input))
-
-
-def init_weights_xavier(m):
-    if type(m) == nn.Linear:
-        if hasattr(m, 'weight'):
-            nn.init.xavier_normal_(m.weight)
-
-def sine_init(m):
-    with torch.no_grad():
-        if hasattr(m, 'weight'):
-            num_input = m.weight.size(-1)
-            # See supplement Sec. 1.5 for discussion of factor 30
-            m.weight.uniform_(-np.sqrt(6 / num_input) / 30, np.sqrt(6 / num_input) / 30)
-
-
-def first_layer_sine_init(m):
-    with torch.no_grad():
-        if hasattr(m, 'weight'):
-            num_input = m.weight.size(-1)
-            # See paper sec. 3.2, final paragraph, and supplement Sec. 1.5 for discussion of factor 30
-            m.weight.uniform_(-1 / num_input, 1 / num_input)
+class SineLayer(nn.Module):
+    # See paper sec. 3.2, final paragraph, and supplement Sec. 1.5 for discussion of omega_0.
+    
+    # If is_first=True, omega_0 is a frequency factor which simply multiplies the activations before the 
+    # nonlinearity. Different signals may require different omega_0 in the first layer - this is a 
+    # hyperparameter.
+    
+    # If is_first=False, then the weights will be divided by omega_0 so as to keep the magnitude of 
+    # activations constant, but boost gradients to the weight matrix (see supplement Sec. 1.5)
+    
+    def __init__(self, in_features, out_features, bias=True,
+                 is_first=False, omega_0=30):
+        super().__init__()
+        self.omega_0 = omega_0
+        self.is_first = is_first
+        
+        self.in_features = in_features
+        self.linear = nn.Linear(in_features, out_features, bias=bias)
+        
+        self.init_weights()
+    
+    def init_weights(self):
+        with torch.no_grad():
+            if self.is_first:
+                self.linear.weight.uniform_(-1 / self.in_features, 
+                                             1 / self.in_features)      
+            else:
+                self.linear.weight.uniform_(-np.sqrt(6 / self.in_features) / self.omega_0, 
+                                             np.sqrt(6 / self.in_features) / self.omega_0)
+        
+    def forward(self, input):
+        return torch.sin(self.omega_0 * self.linear(input))
+    
+    def forward_with_intermediate(self, input): 
+        # For visualization of activation distributions
+        intermediate = self.omega_0 * self.linear(input)
+        return torch.sin(intermediate), intermediate
 
 class ImplicitModel(nn.Module):
-    def __init__ (self, opt):
-        super(ImplicitModel, self).__init__()
+    def __init__(self, opt):
+        super().__init__()
+        
         self.opt = opt
-        self.activation_function = torch.sin if opt['activation_function'] \
-            == 'sin' else torch.relu
-        if(opt['use_positional_encoding']):
-            self.positional_encoding = PositionalEncoding(opt)
+        self.net = []
+        self.net.append(SineLayer(opt['n_dims'], opt['nodes_per_layer'], 
+                                  is_first=True, omega_0=30))
 
-        self.head = nn.Linear(opt['n_dims'] if not opt['use_positional_encoding'] else \
-            opt['num_positional_encoding_terms'] * opt['n_dims'] * 2, opt['nodes_per_layer'])
+        for i in range(opt['n_layers']):
+            self.net.append(SineLayer(opt['nodes_per_layer'], opt['nodes_per_layer'], 
+                                      is_first=False, omega_0=30))
 
-        self.body = nn.ModuleList([])
-        for _ in range(opt['n_layers']):
-            self.body.append(nn.Linear(opt['nodes_per_layer'], opt['nodes_per_layer']))
+        self.net.append(SineLayer(opt['nodes_per_layer'], opt['n_dims'], 
+                                      is_first=False, omega_0=30))
         
-        self.tail = nn.Linear(opt['nodes_per_layer'], opt['n_dims'])
-
-        self.head.apply(first_layer_sine_init)
-        self.body.apply(sine_init)
-        self.tail.apply(sine_init)
-
-    def forward(self,x):
-        if(self.opt['use_positional_encoding']):
-            x = self.positional_encoding(x)
-            
-        y_est = self.head(30 * x)
-        y_est = self.activation_function(y_est)
-
-        for i in range(len(self.body)):
-            y_est = self.body[i](y_est)
-            y_est = self.activation_function(30 * y_est)
-        
-        y_est = self.tail(y_est)
-        return y_est
+        self.net = nn.Sequential(*self.net)
+    
+    def forward(self, coords):
+        coords = coords.clone().detach().requires_grad_(True) # allows to take derivative w.r.t. input
+        output = self.net(coords)
+        return output, coords
