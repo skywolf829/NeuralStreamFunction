65a06cdd260affdaef39231cd554eb6e23b10737
Skylar Wurster <swwurster@gmail.com>
2021-11-04 13:58:30 -0400

    Updates

--- Code/ShellScripts/train.sh
+++ Code/ShellScripts/train.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 cd /lus/theta-fs0/projects/DL4VIS/DeepHierarchicalSuperResolution
 
-python -u Code/train.py --n_outputs 1 --n_dims 3 \
---vector_field_name isotropic_coarse_mag.h5 \
+python -u Code/train.py --n_outputs 3 --n_dims 3 \
+--vector_field_name isotropic_coarse_vf.h5 \
 --n_layers 4 --nodes_per_layer 128 \
---save_name isomag_4x128 \
+--save_name isovf_4x128 \
 --points_per_iteration 500000
--- Code/train.py
+++ Code/train.py
@@ -54,6 +54,10 @@ def train_implicit_model(rank, model, dataset, opt):
     if((rank == 0 and opt['train_distributed']) or not opt['train_distributed']):
         writer = SummaryWriter(os.path.join('tensorboard',opt['save_name']))
         gt_img = dataset.get_2D_slice()
+        im_min = gt_img.min()
+        gt_img -= im_min
+        im_max = gt_img.max()
+        gt_img /= im_max
         writer.add_image("Ground Truth", gt_img, 0, dataformats="CHW")
     start_time = time.time()    
     loss_func = nn.L1Loss().to(opt["device"])
@@ -83,17 +87,22 @@ def train_implicit_model(rank, model, dataset, opt):
                 p = PSNR(y_estimated, y, dataset.max()-dataset.min())
                 writer.add_scalar('PSNR', p.item(), iteration)
                 
-                GBytes = (torch.cuda.max_memory_allocated(device=opt['device']) / (1024**3))
+                GBytes = (torch.cuda.max_memory_allocated(device=opt['device']) \
+                    / (1024**3))
                 writer.add_scalar('GPU memory (GB)', GBytes, iteration)
             
             if(iteration % 100 == 0):
                 grid_to_sample = dataset.data.shape[2:]
                 with torch.no_grad():
                     img = model.sample_grid(grid_to_sample)
+                if(im_min < 0):
+                    img -= im_min
+                    img /= im_max
                 #grad_img = model.sample_grid_gradient(grid_to_sample)
-                writer.add_image('Reconstruction', img.clamp(dataset.min(), dataset.max()), iteration, dataformats='WHC')
+                writer.add_image('Reconstruction', img.clamp(0, 1), 
+                    iteration, dataformats='WHC')
     writer.close()
-    
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Train on an input that is 2D')
 
