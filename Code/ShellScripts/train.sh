3138d71ef03475d566e0f958c448f94e947bb606
Skylar Wurster <swwurster@gmail.com>
2021-11-05 11:46:08 -0400

    Update train.sh

--- Code/ShellScripts/train.sh
+++ Code/ShellScripts/train.sh
@@ -5,7 +5,7 @@ python -u Code/train.py --n_outputs 3 --n_dims 2 \
 --signal_file_name cat.h5 \
 --n_layers 4 --nodes_per_layer 128 \
 --save_name cat_4x128 \
---points_per_iteration 1048576 \
+--points_per_iteration 200000 \
 --log_image false --log_gradient false \
 --device cuda:0 --data_device cuda:0 &
 
@@ -13,7 +13,7 @@ python -u Code/train.py --n_outputs 3 --n_dims 2 \
 --signal_file_name cat.h5 \
 --n_layers 4 --nodes_per_layer 256 \
 --save_name cat_4x256 \
---points_per_iteration 1048576 \
+--points_per_iteration 200000 \
 --log_image false --log_gradient false \
 --device cuda:1 --data_device cuda:1 &
 
@@ -21,7 +21,7 @@ python -u Code/train.py --n_outputs 3 --n_dims 2 \
 --signal_file_name cat.h5 \
 --n_layers 4 --nodes_per_layer 512 \
 --save_name cat_4x512 \
---points_per_iteration 1048576 \
+--points_per_iteration 200000 \
 --log_image false --log_gradient false \
 --device cuda:2 --data_device cuda:2 &
 
@@ -29,7 +29,7 @@ python -u Code/train.py --n_outputs 3 --n_dims 2 \
 --signal_file_name cat.h5 \
 --n_layers 4 --nodes_per_layer 1024 \
 --save_name cat_4x1024 \
---points_per_iteration 1048576 \
+--points_per_iteration 200000 \
 --log_image false --log_gradient false \
 --device cuda:3 --data_device cuda:3 &
 
@@ -40,7 +40,7 @@ python -u Code/train.py --n_outputs 3 --n_dims 2 \
 --signal_file_name cat.h5 \
 --n_layers 6 --nodes_per_layer 128 \
 --save_name cat_6x128 \
---points_per_iteration 1048576 \
+--points_per_iteration 200000 \
 --log_image false --log_gradient false \
 --device cuda:4 --data_device cuda:4 &
 
@@ -48,7 +48,7 @@ python -u Code/train.py --n_outputs 3 --n_dims 2 \
 --signal_file_name cat.h5 \
 --n_layers 6 --nodes_per_layer 256 \
 --save_name cat_6x256 \
---points_per_iteration 1048576 \
+--points_per_iteration 200000 \
 --log_image false --log_gradient false \
 --device cuda:5 --data_device cuda:5 &
 
@@ -56,7 +56,7 @@ python -u Code/train.py --n_outputs 3 --n_dims 2 \
 --signal_file_name cat.h5 \
 --n_layers 6 --nodes_per_layer 512 \
 --save_name cat_6x512 \
---points_per_iteration 1048576 \
+--points_per_iteration 200000 \
 --log_image false --log_gradient false \
 --device cuda:6 --data_device cuda:6 &
 
@@ -64,7 +64,7 @@ python -u Code/train.py --n_outputs 3 --n_dims 2 \
 --signal_file_name cat.h5 \
 --n_layers 6 --nodes_per_layer 1024 \
 --save_name cat_6x1024 \
---points_per_iteration 1048576 \
+--points_per_iteration 200000 \
 --log_image false --log_gradient false \
 --device cuda:7 --data_device cuda:7
 
@@ -75,7 +75,7 @@ python -u Code/train.py --n_outputs 3 --n_dims 2 \
 --signal_file_name cat.h5 \
 --n_layers 8 --nodes_per_layer 128 \
 --save_name cat_8x128 \
---points_per_iteration 1048576 \
+--points_per_iteration 200000 \
 --log_image false --log_gradient false \
 --device cuda:0 --data_device cuda:0 &
 
@@ -83,7 +83,7 @@ python -u Code/train.py --n_outputs 3 --n_dims 2 \
 --signal_file_name cat.h5 \
 --n_layers 8 --nodes_per_layer 256 \
 --save_name cat_8x256 \
---points_per_iteration 1048576 \
+--points_per_iteration 200000 \
 --log_image false --log_gradient false \
 --device cuda:1 --data_device cuda:1 &
 
@@ -91,7 +91,7 @@ python -u Code/train.py --n_outputs 3 --n_dims 2 \
 --signal_file_name cat.h5 \
 --n_layers 8 --nodes_per_layer 512 \
 --save_name cat_8x512 \
---points_per_iteration 1048576 \
+--points_per_iteration 200000 \
 --log_image false --log_gradient false \
 --device cuda:2 --data_device cuda:2 &
 
@@ -99,7 +99,7 @@ python -u Code/train.py --n_outputs 3 --n_dims 2 \
 --signal_file_name cat.h5 \
 --n_layers 8 --nodes_per_layer 1024 \
 --save_name cat_8x1024 \
---points_per_iteration 1048576 \
+--points_per_iteration 200000 \
 --log_image false --log_gradient false \
 --device cuda:3 --data_device cuda:3 &
 
@@ -110,7 +110,7 @@ python -u Code/train.py --n_outputs 3 --n_dims 2 \
 --signal_file_name cat.h5 \
 --n_layers 10 --nodes_per_layer 128 \
 --save_name cat_10x128 \
---points_per_iteration 1048576 \
+--points_per_iteration 200000 \
 --log_image false --log_gradient false \
 --device cuda:4 --data_device cuda:4 &
 
@@ -118,7 +118,7 @@ python -u Code/train.py --n_outputs 3 --n_dims 2 \
 --signal_file_name cat.h5 \
 --n_layers 10 --nodes_per_layer 256 \
 --save_name cat_10x256 \
---points_per_iteration 1048576 \
+--points_per_iteration 200000 \
 --log_image false --log_gradient false \
 --device cuda:5 --data_device cuda:5 &
 
@@ -126,7 +126,7 @@ python -u Code/train.py --n_outputs 3 --n_dims 2 \
 --signal_file_name cat.h5 \
 --n_layers 10 --nodes_per_layer 512 \
 --save_name cat_10x512 \
---points_per_iteration 1048576 \
+--points_per_iteration 200000 \
 --log_image false --log_gradient false \
 --device cuda:6 --data_device cuda:6 &
 
@@ -134,7 +134,7 @@ python -u Code/train.py --n_outputs 3 --n_dims 2 \
 --signal_file_name cat.h5 \
 --n_layers 10 --nodes_per_layer 1024 \
 --save_name cat_10x1024 \
---points_per_iteration 1048576 \
+--points_per_iteration 200000 \
 --log_image false --log_gradient false \
 --device cuda:7 --data_device cuda:7
 
