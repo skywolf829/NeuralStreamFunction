d73f5a8aa810de1e27cc47ad9f2254a8a71ae221
Skylar Wurster <swwurster@gmail.com>
2021-11-05 14:19:35 -0400

    Update train.sh

--- Code/ShellScripts/train.sh
+++ Code/ShellScripts/train.sh
@@ -2,7 +2,7 @@
 cd /lus/theta-fs0/projects/DL4VIS/ImplicitStreamFunction
 
 python -u Code/train.py --n_outputs 3 --n_dims 3 \
---signal_file_name isotropic_coarse.h5 \
+--signal_file_name isotropic_coarse_vf.h5 \
 --n_layers 4 --nodes_per_layer 128 \
 --save_name isotropic_coarse_4x128 \
 --points_per_iteration 200000 \
@@ -10,7 +10,7 @@ python -u Code/train.py --n_outputs 3 --n_dims 3 \
 --device cuda:0 --data_device cuda:0 &
 
 python -u Code/train.py --n_outputs 3 --n_dims 3 \
---signal_file_name isotropic_coarse.h5 \
+--signal_file_name isotropic_coarse_vf.h5 \
 --n_layers 4 --nodes_per_layer 256 \
 --save_name isotropic_coarse4x256 \
 --points_per_iteration 200000 \
@@ -18,7 +18,7 @@ python -u Code/train.py --n_outputs 3 --n_dims 3 \
 --device cuda:1 --data_device cuda:1 &
 
 python -u Code/train.py --n_outputs 3 --n_dims 3 \
---signal_file_name isotropic_coarse.h5 \
+--signal_file_name isotropic_coarse_vf.h5 \
 --n_layers 4 --nodes_per_layer 512 \
 --save_name isotropic_coarse4x512 \
 --points_per_iteration 200000 \
@@ -26,7 +26,7 @@ python -u Code/train.py --n_outputs 3 --n_dims 3 \
 --device cuda:2 --data_device cuda:2 &
 
 python -u Code/train.py --n_outputs 3 --n_dims 3 \
---signal_file_name isotropic_coarse.h5 \
+--signal_file_name isotropic_coarse_vf.h5 \
 --n_layers 4 --nodes_per_layer 1024 \
 --save_name isotropic_coarse4x1024 \
 --points_per_iteration 200000 \
@@ -37,7 +37,7 @@ python -u Code/train.py --n_outputs 3 --n_dims 3 \
 ###########################################################
 
 python -u Code/train.py --n_outputs 3 --n_dims 3 \
---signal_file_name isotropic_coarse.h5 \
+--signal_file_name isotropic_coarse_vf.h5 \
 --n_layers 6 --nodes_per_layer 128 \
 --save_name isotropic_coarse6x128 \
 --points_per_iteration 200000 \
@@ -45,7 +45,7 @@ python -u Code/train.py --n_outputs 3 --n_dims 3 \
 --device cuda:4 --data_device cuda:4 &
 
 python -u Code/train.py --n_outputs 3 --n_dims 3 \
---signal_file_name isotropic_coarse.h5 \
+--signal_file_name isotropic_coarse_vf.h5 \
 --n_layers 6 --nodes_per_layer 256 \
 --save_name isotropic_coarse6x256 \
 --points_per_iteration 200000 \
@@ -53,7 +53,7 @@ python -u Code/train.py --n_outputs 3 --n_dims 3 \
 --device cuda:5 --data_device cuda:5 &
 
 python -u Code/train.py --n_outputs 3 --n_dims 3 \
---signal_file_name isotropic_coarse.h5 \
+--signal_file_name isotropic_coarse_vf.h5 \
 --n_layers 6 --nodes_per_layer 512 \
 --save_name isotropic_coarse6x512 \
 --points_per_iteration 200000 \
@@ -61,7 +61,7 @@ python -u Code/train.py --n_outputs 3 --n_dims 3 \
 --device cuda:6 --data_device cuda:6 &
 
 python -u Code/train.py --n_outputs 3 --n_dims 3 \
---signal_file_name isotropic_coarse.h5 \
+--signal_file_name isotropic_coarse_vf.h5 \
 --n_layers 6 --nodes_per_layer 1024 \
 --save_name isotropic_coarse6x1024 \
 --points_per_iteration 200000 \
@@ -72,7 +72,7 @@ python -u Code/train.py --n_outputs 3 --n_dims 3 \
 ###########################################################
 
 python -u Code/train.py --n_outputs 3 --n_dims 3 \
---signal_file_name isotropic_coarse.h5 \
+--signal_file_name isotropic_coarse_vf.h5 \
 --n_layers 8 --nodes_per_layer 128 \
 --save_name isotropic_coarse8x128 \
 --points_per_iteration 200000 \
@@ -80,7 +80,7 @@ python -u Code/train.py --n_outputs 3 --n_dims 3 \
 --device cuda:0 --data_device cuda:0 &
 
 python -u Code/train.py --n_outputs 3 --n_dims 3 \
---signal_file_name isotropic_coarse.h5 \
+--signal_file_name isotropic_coarse_vf.h5 \
 --n_layers 8 --nodes_per_layer 256 \
 --save_name isotropic_coarse8x256 \
 --points_per_iteration 200000 \
@@ -88,7 +88,7 @@ python -u Code/train.py --n_outputs 3 --n_dims 3 \
 --device cuda:1 --data_device cuda:1 &
 
 python -u Code/train.py --n_outputs 3 --n_dims 3 \
---signal_file_name isotropic_coarse.h5 \
+--signal_file_name isotropic_coarse_vf.h5 \
 --n_layers 8 --nodes_per_layer 512 \
 --save_name isotropic_coarse8x512 \
 --points_per_iteration 200000 \
@@ -96,7 +96,7 @@ python -u Code/train.py --n_outputs 3 --n_dims 3 \
 --device cuda:2 --data_device cuda:2 &
 
 python -u Code/train.py --n_outputs 3 --n_dims 3 \
---signal_file_name isotropic_coarse.h5 \
+--signal_file_name isotropic_coarse_vf.h5 \
 --n_layers 8 --nodes_per_layer 1024 \
 --save_name isotropic_coarse8x1024 \
 --points_per_iteration 200000 \
@@ -107,7 +107,7 @@ python -u Code/train.py --n_outputs 3 --n_dims 3 \
 ###########################################################
 
 python -u Code/train.py --n_outputs 3 --n_dims 3 \
---signal_file_name isotropic_coarse.h5 \
+--signal_file_name isotropic_coarse_vf.h5 \
 --n_layers 10 --nodes_per_layer 128 \
 --save_name isotropic_coarse10x128 \
 --points_per_iteration 200000 \
@@ -115,7 +115,7 @@ python -u Code/train.py --n_outputs 3 --n_dims 3 \
 --device cuda:4 --data_device cuda:4 &
 
 python -u Code/train.py --n_outputs 3 --n_dims 3 \
---signal_file_name isotropic_coarse.h5 \
+--signal_file_name isotropic_coarse_vf.h5 \
 --n_layers 10 --nodes_per_layer 256 \
 --save_name isotropic_coarse10x256 \
 --points_per_iteration 200000 \
@@ -123,7 +123,7 @@ python -u Code/train.py --n_outputs 3 --n_dims 3 \
 --device cuda:5 --data_device cuda:5 &
 
 python -u Code/train.py --n_outputs 3 --n_dims 3 \
---signal_file_name isotropic_coarse.h5 \
+--signal_file_name isotropic_coarse_vf.h5 \
 --n_layers 10 --nodes_per_layer 512 \
 --save_name isotropic_coarse10x512 \
 --points_per_iteration 200000 \
@@ -131,7 +131,7 @@ python -u Code/train.py --n_outputs 3 --n_dims 3 \
 --device cuda:6 --data_device cuda:6 &
 
 python -u Code/train.py --n_outputs 3 --n_dims 3 \
---signal_file_name isotropic_coarse.h5 \
+--signal_file_name isotropic_coarse_vf.h5 \
 --n_layers 10 --nodes_per_layer 1024 \
 --save_name isotropic_coarse10x1024 \
 --points_per_iteration 200000 \
