f8d9b8f8cdc753958f79b2c5dbd06025e8afa4f1
Skylar Wurster <swwurster@gmail.com>
2021-11-23 14:59:45 -0500

    Update train_isabella.sh

--- Code/ShellScripts/train_isabella.sh
+++ Code/ShellScripts/train_isabella.sh
@@ -6,6 +6,6 @@ python -u Code/train.py --n_outputs 4 --n_dims 3 \
 --n_layers 6 --nodes_per_layer 512 \
 --save_name isabella_6x512 \
 --points_per_iteration 100000 \
---log_image false --log_gradient false \
+--log_image true --log_gradient false \
 --loss l1occupancy \
 --device cuda:0 --data_device cuda:0 
