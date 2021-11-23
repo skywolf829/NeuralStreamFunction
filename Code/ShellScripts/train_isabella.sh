ddd0b0d5194de7c868795984617d4cb0b0dceef7
Skylar Wurster <swwurster@gmail.com>
2021-11-23 18:38:36 -0500

    Update train_isabella.sh

--- Code/ShellScripts/train_isabella.sh
+++ Code/ShellScripts/train_isabella.sh
@@ -3,8 +3,8 @@ cd /lus/theta-fs0/projects/DL4VIS/ImplicitStreamFunction
 
 python -u Code/train.py --n_outputs 3 --n_dims 3 \
 --signal_file_name isabella13.h5 \
---n_layers 8 --nodes_per_layer 1024 \
---save_name isabella_8x1024 \
+--n_layers 6 --nodes_per_layer 512 \
+--save_name isabella_6x512 \
 --points_per_iteration 250000 \
 --log_image true --log_gradient false \
 --loss l1 \
