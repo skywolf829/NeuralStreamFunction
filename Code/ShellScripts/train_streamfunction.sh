6483f7692d6af7f2bacf0443166bb2cac3fe8877
Skylar Wurster <swwurster@gmail.com>
2022-02-06 16:38:44 -0500

    Update train_streamfunction.sh

--- Code/ShellScripts/train_streamfunction.sh
+++ Code/ShellScripts/train_streamfunction.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 cd /lus/theta-fs0/projects/DL4VIS/ImplicitStreamFunction
 python3 -u Code/train.py --n_outputs 1 --n_dims 3 \
---signal_file_name synthetic_VF2.h5 \
+--signal_file_name synthetic_VF3.h5 \
 --n_layers 4 --nodes_per_layer 128 \
---save_name synth2 \
+--save_name synth3 \
 --points_per_iteration 200000 \
 --iterations 10000 \
 --fit_gradient true \
@@ -12,9 +12,9 @@ python3 -u Code/train.py --n_outputs 1 --n_dims 3 \
 --device cuda:0 --data_device cuda:0 &
 
 python -u Code/train.py --n_outputs 1 --n_dims 3 \
---signal_file_name synthetic_VF2_binormal.h5 \
+--signal_file_name synthetic_VF3_binormal.h5 \
 --n_layers 4 --nodes_per_layer 128 \
---save_name synth2_binormal \
+--save_name synth3_binormal \
 --points_per_iteration 200000 \
 --iterations 10000 \
 --fit_gradient true \
@@ -23,9 +23,9 @@ python -u Code/train.py --n_outputs 1 --n_dims 3 \
 --device cuda:1 --data_device cuda:1 &
 
 python -u Code/train.py --n_outputs 1 --n_dims 3 \
---signal_file_name synthetic_VF2_normal.h5 \
+--signal_file_name synthetic_VF3_normal.h5 \
 --n_layers 4 --nodes_per_layer 128 \
---save_name synth2_normal \
+--save_name synth3_normal \
 --points_per_iteration 200000 \
 --iterations 10000 \
 --fit_gradient true \
