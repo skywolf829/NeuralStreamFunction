e683763d4a09e395e27293127275aeca34133bdb
Skylar Wurster <swwurster@gmail.com>
2022-02-08 18:35:20 -0500

    Update train_streamfunction.sh

--- Code/ShellScripts/train_streamfunction.sh
+++ Code/ShellScripts/train_streamfunction.sh
@@ -2,9 +2,9 @@
 cd /lus/theta-fs0/projects/DL4VIS/ImplicitStreamFunction
 
 python3 -u Code/train.py --n_outputs 1 --n_dims 3 \
---signal_file_name synthetic_VF2.h5 \
+--signal_file_name synthetic_VF1.h5 \
 --n_layers 4 --nodes_per_layer 128 \
---save_name synth2 \
+--save_name synth1 \
 --points_per_iteration 100000 \
 --iterations 10000 \
 --fit_gradient true \
@@ -13,9 +13,9 @@ python3 -u Code/train.py --n_outputs 1 --n_dims 3 \
 --device cuda:0 --data_device cuda:0 &
 
 python3 -u Code/train.py --n_outputs 1 --n_dims 3 \
---signal_file_name synthetic_VF2_normal.h5 \
+--signal_file_name synthetic_VF1_normal.h5 \
 --n_layers 4 --nodes_per_layer 128 \
---save_name synth2_normal \
+--save_name synth1_normal \
 --points_per_iteration 100000 \
 --iterations 10000 \
 --fit_gradient true \
@@ -24,34 +24,12 @@ python3 -u Code/train.py --n_outputs 1 --n_dims 3 \
 --device cuda:1 --data_device cuda:1 &
 
 python3 -u Code/train.py --n_outputs 1 --n_dims 3 \
---signal_file_name synthetic_VF2_binormal.h5 \
+--signal_file_name synthetic_VF1_binormal.h5 \
 --n_layers 4 --nodes_per_layer 128 \
---save_name synth2_binormal \
+--save_name synth1_binormal \
 --points_per_iteration 100000 \
 --iterations 10000 \
 --fit_gradient true \
 --loss magangle_same --lr 5e-5 \
 --log_image false --log_gradient false \
---device cuda:2 --data_device cuda:2 &
-
-python3 -u Code/train.py --n_outputs 1 --n_dims 3 \
---signal_file_name synthetic_VF3_binormal.h5 \
---n_layers 4 --nodes_per_layer 128 \
---save_name synth3_binormal \
---points_per_iteration 100000 \
---iterations 10000 \
---fit_gradient true \
---loss magangle_same --lr 5e-5 \
---log_image false --log_gradient false \
---device cuda:3 --data_device cuda:3 &
-
-python3 -u Code/train.py --n_outputs 1 --n_dims 3 \
---signal_file_name synthetic_VF3_normal.h5 \
---n_layers 4 --nodes_per_layer 128 \
---save_name synth3_normal \
---points_per_iteration 100000 \
---iterations 10000 \
---fit_gradient true \
---loss magangle_same --lr 5e-5 \
---log_image false --log_gradient false \
---device cuda:4 --data_device cuda:4 
+--device cuda:2 --data_device cuda:2 
