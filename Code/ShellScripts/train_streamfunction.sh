164edf1f4c26e41aed3c052ef831bdd3fd8b8bbd
Skylar Wurster <swwurster@gmail.com>
2022-02-08 19:49:50 -0500

    Update train_streamfunction.sh

--- Code/ShellScripts/train_streamfunction.sh
+++ Code/ShellScripts/train_streamfunction.sh
@@ -3,11 +3,11 @@ cd /lus/theta-fs0/projects/DL4VIS/ImplicitStreamFunction
 
 python3 -u Code/train.py --n_outputs 1 --n_dims 3 \
 --signal_file_name synthetic_VF3.h5 \
---save_name synth3_dual_streamfunctions \
+--save_name synth3_dual_streamfunction \
 --n_layers 4 --nodes_per_layer 128 \
 --points_per_iteration 100000 \
 --iterations 10000 \
---dual_streamfunctions true \
+--dual_streamfunction true \
 --loss magangle_same --lr 5e-5 \
 --log_image false --log_gradient false \
 --device cuda:0 --data_device cuda:0 
