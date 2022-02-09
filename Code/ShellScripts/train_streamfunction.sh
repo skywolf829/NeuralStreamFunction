3464a6a639b84888095540bb82e90af4e99df6c7
Skylar Wurster <swwurster@gmail.com>
2022-02-08 19:50:22 -0500

    Update train_streamfunction.sh

--- Code/ShellScripts/train_streamfunction.sh
+++ Code/ShellScripts/train_streamfunction.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd /lus/theta-fs0/projects/DL4VIS/ImplicitStreamFunction
 
-python3 -u Code/train.py --n_outputs 1 --n_dims 3 \
+python3 -u Code/train.py --n_outputs 2 --n_dims 3 \
 --signal_file_name synthetic_VF3.h5 \
 --save_name synth3_dual_streamfunction \
 --n_layers 4 --nodes_per_layer 128 \
