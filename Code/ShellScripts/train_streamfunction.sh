a7ec02335efccf5eef3f271a4ac946e6c31e0e3d
Skylar Wurster <swwurster@gmail.com>
2022-02-17 22:28:18 -0500

    Update train_streamfunction.sh

--- Code/ShellScripts/train_streamfunction.sh
+++ Code/ShellScripts/train_streamfunction.sh
@@ -2,8 +2,8 @@
 cd /lus/theta-fs0/projects/DL4VIS/ImplicitStreamFunction
 
 python -u Code/train.py --n_outputs 1 --n_dims 3 \
---signal_file_name tornado3d.h5 \
---save_name tornado_normal \
+--signal_file_name synthetic_VF2.h5 \
+--save_name synthetic2_normal \
 --normal true \
 --n_layers 4 --nodes_per_layer 128 \
 --points_per_iteration 100000 \
@@ -14,8 +14,8 @@ python -u Code/train.py --n_outputs 1 --n_dims 3 \
 --device cuda:0 --data_device cuda:0 &
 
 python -u Code/train.py --n_outputs 1 --n_dims 3 \
---signal_file_name tornado3d.h5 \
---save_name tornado_orthogonal \
+--signal_file_name synthetic_VF2.h5 \
+--save_name synthetic2_orthogonal \
 --normal false \
 --n_layers 4 --nodes_per_layer 128 \
 --points_per_iteration 100000 \
@@ -26,8 +26,8 @@ python -u Code/train.py --n_outputs 1 --n_dims 3 \
 --device cuda:1 --data_device cuda:1 &
 
 python -u Code/train.py --n_outputs 1 --n_dims 3 \
---signal_file_name tornado3d.h5 \
---save_name tornado_binormal \
+--signal_file_name synthetic_VF2.h5 \
+--save_name synthetic2_binormal \
 --binormal true \
 --n_layers 4 --nodes_per_layer 128 \
 --points_per_iteration 100000 \
@@ -38,8 +38,8 @@ python -u Code/train.py --n_outputs 1 --n_dims 3 \
 --device cuda:2 --data_device cuda:2 &
 
 python -u Code/train.py --n_outputs 1 --n_dims 3 \
---signal_file_name synthetic_VF1.h5 \
---save_name synthetic_normal \
+--signal_file_name synthetic_VF3.h5 \
+--save_name synthetic3_normal \
 --normal true \
 --n_layers 4 --nodes_per_layer 128 \
 --points_per_iteration 100000 \
@@ -50,8 +50,8 @@ python -u Code/train.py --n_outputs 1 --n_dims 3 \
 --device cuda:3 --data_device cuda:3 &
 
 python -u Code/train.py --n_outputs 1 --n_dims 3 \
---signal_file_name synthetic_VF1.h5 \
---save_name synthetic1_orthogonal \
+--signal_file_name synthetic_VF3.h5 \
+--save_name synthetic3_orthogonal \
 --normal false \
 --n_layers 4 --nodes_per_layer 128 \
 --points_per_iteration 100000 \
@@ -62,8 +62,8 @@ python -u Code/train.py --n_outputs 1 --n_dims 3 \
 --device cuda:4 --data_device cuda:4 &
 
 python -u Code/train.py --n_outputs 1 --n_dims 3 \
---signal_file_name synthetic_VF1.h5 \
---save_name synthetic1_binormal \
+--signal_file_name synthetic_VF3.h5 \
+--save_name synthetic3_binormal \
 --binormal true \
 --n_layers 4 --nodes_per_layer 128 \
 --points_per_iteration 100000 \
