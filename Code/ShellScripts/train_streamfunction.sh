5e71bf2ce9a0cb18f7e572f42b95b7d4269ce914
Skylar Wurster <swwurster@gmail.com>
2022-02-17 22:37:02 -0500

    Update train_streamfunction.sh

--- Code/ShellScripts/train_streamfunction.sh
+++ Code/ShellScripts/train_streamfunction.sh
@@ -2,8 +2,8 @@
 cd /lus/theta-fs0/projects/DL4VIS/ImplicitStreamFunction
 
 python -u Code/train.py --n_outputs 1 --n_dims 3 \
---signal_file_name synthetic_VF1.h5 \
---save_name synthetic1_normal \
+--signal_file_name isabel.h5 \
+--save_name isabel_normal_4x128 \
 --normal true \
 --n_layers 4 --nodes_per_layer 128 \
 --points_per_iteration 100000 \
@@ -12,3 +12,15 @@ python -u Code/train.py --n_outputs 1 --n_dims 3 \
 --loss angle_parallel --lr 5e-5 \
 --log_image false --log_gradient false \
 --device cuda:3 --data_device cuda:3 &
+
+python -u Code/train.py --n_outputs 1 --n_dims 3 \
+--signal_file_name isabel.h5 \
+--save_name isabel_normal_4x512 \
+--normal true \
+--n_layers 4 --nodes_per_layer 512 \
+--points_per_iteration 100000 \
+--iterations 10000 \
+--fit_gradient true \
+--loss angle_parallel --lr 5e-5 \
+--log_image false --log_gradient false \
+--device cuda:1 --data_device cuda:1 
