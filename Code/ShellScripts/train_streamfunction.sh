4ea6ddaa82969404bf3cdc8ad2d71768a509dc71
Skylar Wurster <swwurster@gmail.com>
2022-02-18 13:30:37 -0500

    Update train_streamfunction.sh

--- Code/ShellScripts/train_streamfunction.sh
+++ Code/ShellScripts/train_streamfunction.sh
@@ -3,9 +3,9 @@ cd /lus/theta-fs0/projects/DL4VIS/ImplicitStreamFunction
 
 python -u Code/train.py --n_outputs 1 --n_dims 3 \
 --signal_file_name isabel.h5 \
---save_name isabel \
+--save_name isabel_4x256 \
 --normal false \
---n_layers 4 --nodes_per_layer 128 \
+--n_layers 4 --nodes_per_layer 256 \
 --points_per_iteration 100000 \
 --iterations 10000 \
 --fit_gradient true \
@@ -15,9 +15,9 @@ python -u Code/train.py --n_outputs 1 --n_dims 3 \
 
 python -u Code/train.py --n_outputs 1 --n_dims 3 \
 --signal_file_name isabel.h5 \
---save_name isabel_normal \
---normal true \
---n_layers 4 --nodes_per_layer 128 \
+--save_name --save_name isabel_4x512 \
+--normal false \
+--n_layers 4 --nodes_per_layer 512 \
 --points_per_iteration 100000 \
 --iterations 10000 \
 --fit_gradient true \
@@ -27,9 +27,9 @@ python -u Code/train.py --n_outputs 1 --n_dims 3 \
 
 python -u Code/train.py --n_outputs 1 --n_dims 3 \
 --signal_file_name isabel.h5 \
---save_name isabel_binormal \
---binormal true \
---n_layers 4 --nodes_per_layer 128 \
+--save_name --save_name isabel_6x512 \
+--normal false \
+--n_layers 6 --nodes_per_layer 512 \
 --points_per_iteration 100000 \
 --iterations 10000 \
 --fit_gradient true \
@@ -39,12 +39,24 @@ python -u Code/train.py --n_outputs 1 --n_dims 3 \
 
 python -u Code/train.py --n_outputs 1 --n_dims 3 \
 --signal_file_name isabel.h5 \
---save_name isabel_orthogonal \
---binormal false \
---n_layers 4 --nodes_per_layer 128 \
+--save_name --save_name isabel_8x512 \
+--normal false \
+--n_layers 8 --nodes_per_layer 512 \
 --points_per_iteration 100000 \
 --iterations 10000 \
 --fit_gradient true \
---loss angle_orthogonal --lr 5e-5 \
+--loss angle_parallel --lr 5e-5 \
 --log_image false --log_gradient false \
 --device cuda:3 --data_device cuda:3 &
+
+python -u Code/train.py --n_outputs 1 --n_dims 3 \
+--signal_file_name isabel.h5 \
+--save_name --save_name isabel_6x1024 \
+--normal false \
+--n_layers 6 --nodes_per_layer 1024 \
+--points_per_iteration 100000 \
+--iterations 10000 \
+--fit_gradient true \
+--loss angle_parallel --lr 5e-5 \
+--log_image false --log_gradient false \
+--device cuda:4 --data_device cuda:4 &
