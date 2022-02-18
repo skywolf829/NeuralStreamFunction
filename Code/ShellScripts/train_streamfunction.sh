24526b2242c09bfdc6e73efd520e2cd6c8ec478f
Skylar Wurster <swwurster@gmail.com>
2022-02-18 13:12:25 -0500

    Update train_streamfunction.sh

--- Code/ShellScripts/train_streamfunction.sh
+++ Code/ShellScripts/train_streamfunction.sh
@@ -2,8 +2,8 @@
 cd /lus/theta-fs0/projects/DL4VIS/ImplicitStreamFunction
 
 python -u Code/train.py --n_outputs 1 --n_dims 3 \
---signal_file_name synthetic_VF3.h5 \
---save_name synthetic3 \
+--signal_file_name isabel.h5 \
+--save_name isabel \
 --normal false \
 --n_layers 4 --nodes_per_layer 128 \
 --points_per_iteration 100000 \
@@ -14,8 +14,8 @@ python -u Code/train.py --n_outputs 1 --n_dims 3 \
 --device cuda:0 --data_device cuda:0 &
 
 python -u Code/train.py --n_outputs 1 --n_dims 3 \
---signal_file_name synthetic_VF3.h5 \
---save_name synthetic3_normal \
+--signal_file_name isabel.h5 \
+--save_name isabel_normal \
 --normal true \
 --n_layers 4 --nodes_per_layer 128 \
 --points_per_iteration 100000 \
@@ -26,8 +26,8 @@ python -u Code/train.py --n_outputs 1 --n_dims 3 \
 --device cuda:1 --data_device cuda:1 &
 
 python -u Code/train.py --n_outputs 1 --n_dims 3 \
---signal_file_name synthetic_VF3.h5 \
---save_name synthetic3_binormal \
+--signal_file_name isabel.h5 \
+--save_name isabel_binormal \
 --binormal true \
 --n_layers 4 --nodes_per_layer 128 \
 --points_per_iteration 100000 \
@@ -38,8 +38,8 @@ python -u Code/train.py --n_outputs 1 --n_dims 3 \
 --device cuda:2 --data_device cuda:2 &
 
 python -u Code/train.py --n_outputs 1 --n_dims 3 \
---signal_file_name synthetic_VF3.h5 \
---save_name synthetic3_orthogonal \
+--signal_file_name isabel.h5 \
+--save_name isabel_orthogonal \
 --binormal false \
 --n_layers 4 --nodes_per_layer 128 \
 --points_per_iteration 100000 \
@@ -48,51 +48,3 @@ python -u Code/train.py --n_outputs 1 --n_dims 3 \
 --loss angle_orthogonal --lr 5e-5 \
 --log_image false --log_gradient false \
 --device cuda:3 --data_device cuda:3 &
-
-python -u Code/train.py --n_outputs 1 --n_dims 3 \
---signal_file_name tornado3d.h5 \
---save_name tornado \
---normal false \
---n_layers 4 --nodes_per_layer 128 \
---points_per_iteration 100000 \
---iterations 10000 \
---fit_gradient true \
---loss angle_parallel --lr 5e-5 \
---log_image false --log_gradient false \
---device cuda:4 --data_device cuda:4 &
-
-python -u Code/train.py --n_outputs 1 --n_dims 3 \
---signal_file_name tornado3d.h5 \
---save_name tornado_normal \
---normal true \
---n_layers 4 --nodes_per_layer 128 \
---points_per_iteration 100000 \
---iterations 10000 \
---fit_gradient true \
---loss angle_parallel --lr 5e-5 \
---log_image false --log_gradient false \
---device cuda:5 --data_device cuda:5 &
-
-python -u Code/train.py --n_outputs 1 --n_dims 3 \
---signal_file_name tornado3d.h5 \
---save_name tornado_binormal \
---binormal true \
---n_layers 4 --nodes_per_layer 128 \
---points_per_iteration 100000 \
---iterations 10000 \
---fit_gradient true \
---loss angle_parallel --lr 5e-5 \
---log_image false --log_gradient false \
---device cuda:6 --data_device cuda:6 &
-
-python -u Code/train.py --n_outputs 1 --n_dims 3 \
---signal_file_name tornado3d.h5 \
---save_name tornado_orthogonal \
---binormal false \
---n_layers 4 --nodes_per_layer 128 \
---points_per_iteration 100000 \
---iterations 10000 \
---fit_gradient true \
---loss angle_orthogonal --lr 5e-5 \
---log_image false --log_gradient false \
---device cuda:7 --data_device cuda:7 &
