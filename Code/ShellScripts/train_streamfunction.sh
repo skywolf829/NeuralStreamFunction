77d232422c52d66241ecafbfaa2b2a56b001ca2c
Skylar Wurster <swwurster@gmail.com>
2022-02-23 13:44:20 -0500

    Updates training script

--- Code/ShellScripts/train_streamfunction.sh
+++ Code/ShellScripts/train_streamfunction.sh
@@ -2,13 +2,97 @@
 cd /lus/theta-fs0/projects/DL4VIS/ImplicitStreamFunction
 
 python -u Code/train.py --n_outputs 2 --n_dims 3 \
---signal_file_name isabel.h5 \
---save_name test \
+--signal_file_name synthetic_VF1.h5 \
+--save_name synthetic1_direction \
 --normal false \
 --n_layers 4 --nodes_per_layer 128 \
 --points_per_iteration 100000 \
 --iterations 10000 \
 --dual_streamfunction true \
---loss angle_parallel --lr 5e-5 \
+--loss angle_same --lr 5e-5 \
 --log_image false --log_gradient false \
---device cuda:0 --data_device cuda:0 
+--device cuda:0 --data_device cuda:0 &
+
+python -u Code/train.py --n_outputs 2 --n_dims 3 \
+--signal_file_name synthetic_VF1.h5 \
+--save_name synthetic1_same \
+--normal false \
+--n_layers 4 --nodes_per_layer 128 \
+--points_per_iteration 100000 \
+--iterations 10000 \
+--dual_streamfunction true \
+--loss l1 --lr 5e-5 \
+--log_image false --log_gradient false \
+--device cuda:1 --data_device cuda:1 &
+
+python -u Code/train.py --n_outputs 2 --n_dims 3 \
+--signal_file_name synthetic_VF2.h5 \
+--save_name synthetic2_direction \
+--normal false \
+--n_layers 4 --nodes_per_layer 128 \
+--points_per_iteration 100000 \
+--iterations 10000 \
+--dual_streamfunction true \
+--loss angle_same --lr 5e-5 \
+--log_image false --log_gradient false \
+--device cuda:2 --data_device cuda:2 &
+
+python -u Code/train.py --n_outputs 2 --n_dims 3 \
+--signal_file_name synthetic_VF2.h5 \
+--save_name synthetic2_same \
+--normal false \
+--n_layers 4 --nodes_per_layer 128 \
+--points_per_iteration 100000 \
+--iterations 10000 \
+--dual_streamfunction true \
+--loss l1 --lr 5e-5 \
+--log_image false --log_gradient false \
+--device cuda:3 --data_device cuda:3 &
+
+python -u Code/train.py --n_outputs 2 --n_dims 3 \
+--signal_file_name synthetic_VF3.h5 \
+--save_name synthetic3_direction \
+--normal false \
+--n_layers 4 --nodes_per_layer 128 \
+--points_per_iteration 100000 \
+--iterations 10000 \
+--dual_streamfunction true \
+--loss angle_same --lr 5e-5 \
+--log_image false --log_gradient false \
+--device cuda:4 --data_device cuda:4 &
+
+python -u Code/train.py --n_outputs 2 --n_dims 3 \
+--signal_file_name synthetic_VF3.h5 \
+--save_name synthetic3_same \
+--normal false \
+--n_layers 4 --nodes_per_layer 128 \
+--points_per_iteration 100000 \
+--iterations 10000 \
+--dual_streamfunction true \
+--loss l1 --lr 5e-5 \
+--log_image false --log_gradient false \
+--device cuda:5 --data_device cuda:5 &
+
+python -u Code/train.py --n_outputs 2 --n_dims 3 \
+--signal_file_name tornado3d.h5 \
+--save_name tornado3d_direction \
+--normal false \
+--n_layers 4 --nodes_per_layer 128 \
+--points_per_iteration 100000 \
+--iterations 10000 \
+--dual_streamfunction true \
+--loss angle_same --lr 5e-5 \
+--log_image false --log_gradient false \
+--device cuda:6 --data_device cuda:6 &
+
+python -u Code/train.py --n_outputs 2 --n_dims 3 \
+--signal_file_name tornado3d.h5 \
+--save_name tornado3d_same \
+--normal false \
+--n_layers 4 --nodes_per_layer 128 \
+--points_per_iteration 100000 \
+--iterations 10000 \
+--dual_streamfunction true \
+--loss l1 --lr 5e-5 \
+--log_image false --log_gradient false \
+--device cuda:7 --data_device cuda:7
