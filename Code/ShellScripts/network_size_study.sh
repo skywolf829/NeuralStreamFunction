05523d4b80998cb9f34d7224c9b47df19ee4b244
Skylar Wurster <swwurster@gmail.com>
2022-02-27 16:12:57 -0500

    Update study script

--- Code/ShellScripts/network_size_study.sh
+++ Code/ShellScripts/network_size_study.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 cd /lus/theta-fs0/projects/DL4VIS/ImplicitStreamFunction
 
-python -u Code/train.py --n_outputs 2 --n_dims 3 \
+python -u Code/train.py --n_outputs 1 --n_dims 3 \
 --signal_file_name isabel.h5 \
---save_name isabel_parallel_large \
---n_layers 8 --nodes_per_layer 512 \
+--save_name network_size_study_isabel_sm \
+--n_layers 4 --nodes_per_layer 128 \
 --points_per_iteration 200000 \
 --iterations 10000 \
---fit_gradient false \
---dual_streamfunction true \
---normal false \
+--fit_gradient true \
+--dual_streamfunction false \
+--normal true \
 --norm false \
 --norm_per_voxel false \
 --loss angle_parallel --lr 5e-5 \
@@ -17,29 +17,29 @@ python -u Code/train.py --n_outputs 2 --n_dims 3 \
 --device cuda:0 --data_device cuda:0 &
 
 python -u Code/train.py --n_outputs 2 --n_dims 3 \
---signal_file_name isabel.h5 \
---save_name isabel_same_large \
---n_layers 8 --nodes_per_layer 512 \
+--signal_file_name tornado3d.h5 \
+--save_name network_size_study_tornado_sm \
+--n_layers 4 --nodes_per_layer 128 \
 --points_per_iteration 200000 \
 --iterations 10000 \
---fit_gradient false \
---dual_streamfunction true \
---normal false \
+--fit_gradient true \
+--dual_streamfunction false \
+--normal true \
 --norm false \
 --norm_per_voxel false \
---loss angle_same --lr 5e-5 \
+--loss angle_parallel --lr 5e-5 \
 --log_image false --log_gradient false \
 --device cuda:1 --data_device cuda:1 &
 
 python -u Code/train.py --n_outputs 2 --n_dims 3 \
 --signal_file_name isabel.h5 \
---save_name isabel_parallel_small \
+--save_name network_size_study_isabel_md \
 --n_layers 6 --nodes_per_layer 256 \
 --points_per_iteration 200000 \
 --iterations 10000 \
---fit_gradient false \
---dual_streamfunction true \
---normal false \
+--fit_gradient true \
+--dual_streamfunction false \
+--normal true \
 --norm false \
 --norm_per_voxel false \
 --loss angle_parallel --lr 5e-5 \
@@ -47,76 +47,76 @@ python -u Code/train.py --n_outputs 2 --n_dims 3 \
 --device cuda:2 --data_device cuda:2 &
 
 python -u Code/train.py --n_outputs 2 --n_dims 3 \
---signal_file_name isabel.h5 \
---save_name isabel_same_small \
+--signal_file_name tornado3d.h5 \
+--save_name network_size_study_tornado_md \
 --n_layers 6 --nodes_per_layer 256 \
 --points_per_iteration 200000 \
 --iterations 10000 \
---fit_gradient false \
---dual_streamfunction true \
---normal false \
+--fit_gradient true \
+--dual_streamfunction false \
+--normal true \
 --norm false \
 --norm_per_voxel false \
---loss angle_same --lr 5e-5 \
+--loss angle_parallel --lr 5e-5 \
 --log_image false --log_gradient false \
 --device cuda:3 --data_device cuda:3 &
 
-python -u Code/train.py --n_outputs 2 --n_dims 3 \
+python -u Code/train.py --n_outputs 1 --n_dims 3 \
 --signal_file_name isabel.h5 \
---save_name isabel_parallel_med \
---n_layers 6 --nodes_per_layer 1024 \
+--save_name network_size_study_isabel_lg \
+--n_layers 6 --nodes_per_layer 512 \
 --points_per_iteration 200000 \
 --iterations 10000 \
---fit_gradient false \
---dual_streamfunction true \
---normal false \
+--fit_gradient true \
+--dual_streamfunction false \
+--normal true \
 --norm false \
 --norm_per_voxel false \
 --loss angle_parallel --lr 5e-5 \
 --log_image false --log_gradient false \
 --device cuda:4 --data_device cuda:4 &
 
-python -u Code/train.py --n_outputs 2 --n_dims 3 \
---signal_file_name isabel.h5 \
---save_name isabel_same_med \
---n_layers 6 --nodes_per_layer 1024 \
+python -u Code/train.py --n_outputs 1 --n_dims 3 \
+--signal_file_name tornado3d.h5 \
+--save_name network_size_study_tornado_lg \
+--n_layers 6 --nodes_per_layer 512 \
 --points_per_iteration 200000 \
 --iterations 10000 \
---fit_gradient false \
---dual_streamfunction true \
---normal false \
+--fit_gradient true \
+--dual_streamfunction false \
+--normal true \
 --norm false \
 --norm_per_voxel false \
---loss angle_same --lr 5e-5 \
+--loss angle_parallel --lr 5e-5 \
 --log_image false --log_gradient false \
 --device cuda:5 --data_device cuda:5 &
 
-python -u Code/train.py --n_outputs 2 --n_dims 3 \
+python -u Code/train.py --n_outputs 1 --n_dims 3 \
 --signal_file_name isabel.h5 \
---save_name isabel_parallel_xl \
---n_layers 8 --nodes_per_layer 1024 \
---points_per_iteration 50000 \
+--save_name network_size_study_isabel_xl \
+--n_layers 8 --nodes_per_layer 512 \
+--points_per_iteration 200000 \
 --iterations 10000 \
---fit_gradient false \
---dual_streamfunction true \
---normal false \
+--fit_gradient true \
+--dual_streamfunction false \
+--normal true \
 --norm false \
 --norm_per_voxel false \
 --loss angle_parallel --lr 5e-5 \
 --log_image false --log_gradient false \
 --device cuda:6 --data_device cuda:6 &
 
-python -u Code/train.py --n_outputs 2 --n_dims 3 \
---signal_file_name isabel.h5 \
---save_name isabel_same_xl \
---n_layers 8 --nodes_per_layer 1024 \
---points_per_iteration 50000 \
+python -u Code/train.py --n_outputs 1 --n_dims 3 \
+--signal_file_name tornado.h5 \
+--save_name network_size_study_tornado_xl \
+--n_layers 8 --nodes_per_layer 512 \
+--points_per_iteration 200000 \
 --iterations 10000 \
---fit_gradient false \
---dual_streamfunction true \
---normal false \
+--fit_gradient true \
+--dual_streamfunction false \
+--normal true \
 --norm false \
 --norm_per_voxel false \
---loss angle_same --lr 5e-5 \
+--loss angle_parallel --lr 5e-5 \
 --log_image false --log_gradient false \
 --device cuda:7 --data_device cuda:7 
