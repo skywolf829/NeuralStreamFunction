b56035b9284debe483560c7c31fed712dbefdbc8
Skylar Wurster <swwurster@gmail.com>
2022-02-27 18:36:19 -0500

    Create testing.sh

---
+++ Code/ShellScripts/testing.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+cd /lus/theta-fs0/projects/DL4VIS/ImplicitStreamFunction
+
+python -u Code/train.py --n_outputs 1 --n_dims 3 \
+--signal_file_name tornado3d.h5 \
+--save_name network_size_study_tornado_xl_residual \
+--n_layers 4 --nodes_per_layer 512 \
+--residual true \
+--points_per_iteration 200000 \
+--iterations 10000 \
+--fit_gradient true \
+--dual_streamfunction false \
+--normal true \
+--norm false \
+--norm_per_voxel false \
+--loss angle_parallel --lr 5e-5 \
+--log_image false --log_gradient false \
+--device cuda:7 --data_device cuda:7 
