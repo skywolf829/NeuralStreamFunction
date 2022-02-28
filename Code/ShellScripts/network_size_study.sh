b64ac0769dec0afafd2b7969ecad3d1577e1541a
Skylar Wurster <swwurster@gmail.com>
2022-02-27 22:39:48 -0500

    Update network_size_study.sh

--- Code/ShellScripts/network_size_study.sh
+++ Code/ShellScripts/network_size_study.sh
@@ -128,3 +128,131 @@ python -u Code/train.py --n_outputs 1 --n_dims 3 \
 --loss angle_parallel --lr 5e-5 \
 --log_image false --log_gradient false \
 --device cuda:7 --data_device cuda:7 
+
+python -u Code/train.py --n_outputs 1 --n_dims 3 \
+--signal_file_name isabel.h5 \
+--save_name network_size_study_isabel_sm \
+--n_layers 2 --nodes_per_layer 128 \
+--residual false \
+--points_per_iteration 200000 \
+--iterations 10000 \
+--fit_gradient true \
+--dual_streamfunction false \
+--normal true \
+--norm false \
+--norm_per_voxel false \
+--loss angle_parallel --lr 5e-5 \
+--log_image false --log_gradient false \
+--device cuda:0 --data_device cuda:0 &
+
+python -u Code/train.py --n_outputs 2 --n_dims 3 \
+--signal_file_name tornado3d.h5 \
+--save_name network_size_study_tornado_sm \
+--n_layers 2 --nodes_per_layer 128 \
+--residual false \
+--points_per_iteration 200000 \
+--iterations 10000 \
+--fit_gradient true \
+--dual_streamfunction false \
+--normal true \
+--norm false \
+--norm_per_voxel false \
+--loss angle_parallel --lr 5e-5 \
+--log_image false --log_gradient false \
+--device cuda:1 --data_device cuda:1 &
+
+python -u Code/train.py --n_outputs 2 --n_dims 3 \
+--signal_file_name isabel.h5 \
+--save_name network_size_study_isabel_md \
+--n_layers 3 --nodes_per_layer 256 \
+--residual false \
+--points_per_iteration 200000 \
+--iterations 10000 \
+--fit_gradient true \
+--dual_streamfunction false \
+--normal true \
+--norm false \
+--norm_per_voxel false \
+--loss angle_parallel --lr 5e-5 \
+--log_image false --log_gradient false \
+--device cuda:2 --data_device cuda:2 &
+
+python -u Code/train.py --n_outputs 2 --n_dims 3 \
+--signal_file_name tornado3d.h5 \
+--save_name network_size_study_tornado_md \
+--n_layers 3 --nodes_per_layer 256 \
+--residual false \
+--points_per_iteration 200000 \
+--iterations 10000 \
+--fit_gradient true \
+--dual_streamfunction false \
+--normal true \
+--norm false \
+--norm_per_voxel false \
+--loss angle_parallel --lr 5e-5 \
+--log_image false --log_gradient false \
+--device cuda:3 --data_device cuda:3 &
+
+python -u Code/train.py --n_outputs 1 --n_dims 3 \
+--signal_file_name isabel.h5 \
+--save_name network_size_study_isabel_lg \
+--n_layers 3 --nodes_per_layer 512 \
+--residual false \
+--points_per_iteration 200000 \
+--iterations 10000 \
+--fit_gradient true \
+--dual_streamfunction false \
+--normal true \
+--norm false \
+--norm_per_voxel false \
+--loss angle_parallel --lr 5e-5 \
+--log_image false --log_gradient false \
+--device cuda:4 --data_device cuda:4 &
+
+python -u Code/train.py --n_outputs 1 --n_dims 3 \
+--signal_file_name tornado3d.h5 \
+--save_name network_size_study_tornado_lg \
+--n_layers 3 --nodes_per_layer 512 \
+--residual false \
+--points_per_iteration 200000 \
+--iterations 10000 \
+--fit_gradient true \
+--dual_streamfunction false \
+--normal true \
+--norm false \
+--norm_per_voxel false \
+--loss angle_parallel --lr 5e-5 \
+--log_image false --log_gradient false \
+--device cuda:5 --data_device cuda:5 &
+
+python -u Code/train.py --n_outputs 1 --n_dims 3 \
+--signal_file_name isabel.h5 \
+--save_name network_size_study_isabel_xl \
+--n_layers 4 --nodes_per_layer 512 \
+--residual false \
+--points_per_iteration 200000 \
+--iterations 10000 \
+--fit_gradient true \
+--dual_streamfunction false \
+--normal true \
+--norm false \
+--norm_per_voxel false \
+--loss angle_parallel --lr 5e-5 \
+--log_image false --log_gradient false \
+--device cuda:6 --data_device cuda:6 &
+
+python -u Code/train.py --n_outputs 1 --n_dims 3 \
+--signal_file_name tornado3d.h5 \
+--save_name network_size_study_tornado_xl \
+--n_layers 4 --nodes_per_layer 512 \
+--points_per_iteration 200000 \
+--residual false \
+--iterations 10000 \
+--fit_gradient true \
+--dual_streamfunction false \
+--normal true \
+--norm false \
+--norm_per_voxel false \
+--loss angle_parallel --lr 5e-5 \
+--log_image false --log_gradient false \
+--device cuda:7 --data_device cuda:7 
