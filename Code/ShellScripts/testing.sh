12a729e0736054d0eaff676e787c0f6436b3454f
Skylar Wurster <swwurster@gmail.com>
2022-02-27 19:39:06 -0500

    Update testing.sh

--- Code/ShellScripts/testing.sh
+++ Code/ShellScripts/testing.sh
@@ -4,7 +4,7 @@ cd /lus/theta-fs0/projects/DL4VIS/ImplicitStreamFunction
 python -u Code/train.py --n_outputs 2 --n_dims 3 \
 --signal_file_name tornado3d.h5 \
 --save_name network_size_study_tornado_deep_residual \
---n_layers 10 --nodes_per_layer 64 \
+--n_layers 10 --nodes_per_layer 128 \
 --residual true \
 --points_per_iteration 200000 \
 --iterations 10000 \
