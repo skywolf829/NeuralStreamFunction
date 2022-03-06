1cbc2faaad1f5f047326370f61ab3a6dcfe8cbd7
Skylar Wurster <swwurster@gmail.com>
2022-03-06 16:19:53 -0500

    Error in train

--- Code/train.py
+++ Code/train.py
@@ -48,7 +48,7 @@ def train_loop(model, dataset, opt):
         if(opt['dual_stream_function'] == "N_parallel"):
             y_estimated = torch.cross(grads_f.detach(), grads_g, dim=1)
             loss = loss_func(grads_f, y[:,3:]) + \
-                angle_parallel_loss(y[:,0:3], y_estimated)
+                angle_same_loss(y[:,0:3], y_estimated)
         elif(opt['dual_stream_function'] == "N_direction"):
             y_estimated = torch.cross(grads_f.detach(), grads_g, dim=1)
             loss = loss_func(grads_f, y[:,3:]) + \
