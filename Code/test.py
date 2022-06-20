f1d89689e4ccc9e731501956e1585856fed862e2
Skylar Wurster <swwurster@gmail.com>
2022-06-20 11:56:18 -0400

    Adds SF test

--- Code/test.py
+++ Code/test.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, division, print_function
 import argparse
 import os
-from Other.utility_functions import nc_to_tensor, PSNR
+from Other.utility_functions import PSNR, tensor_to_cdf, create_path
 from Models.models import load_model
 from Models.options import *
 from Datasets.datasets import Dataset
@@ -47,21 +47,45 @@ def model_reconstruction(model, dataset, opt):
     result = result.to(opt['data_device'])
     p = PSNR(result, data.data)
     print(f"PSNR: {p : 0.02f}")
+    create_path(os.path.join(output_folder, "Reconstruction"))
+    tensor_to_cdf(result, os.path.join(output_folder, "Reconstruction", opt['save_name']+".nc"))
+
+def model_stream_function(model, dataset, opt):
+    grid = list(dataset.data.shape[2:])
+    if("dsfm" in opt['training_mode']): 
+        with torch.no_grad():
+            f = model.sample_grid(grid, max_points=100000)[...,0:1]
+            f = f.permute(3, 0, 1, 2).unsqueeze(0)
+        
+    elif("uvwf" in opt['training_mode']):
+        with torch.no_grad():
+            f = model.sample_grid(grid, max_points = 100000)[...,3:4]
+            f = f.permute(3, 0, 1, 2).unsqueeze(0)
+    
+    f = f.to(opt['data_device'])
+    create_path(os.path.join(output_folder, "StreamFunction"))
+    tensor_to_cdf(f, os.path.join(output_folder, "StreamFunction", opt['save_name']+".nc"))
 
 def perform_tests(model, data, tests, opt):
     if("reconstruction" in tests):
         if(opt['training_mode'] == "dsfm_any" or
-           opt['training_mode'] == "dsfm_parallel" or
-           opt['training_mode'] == "dsfm_direction" or
-           opt['training_mode'] == "uvw" or 
-           opt['training_mode'] == "uvwf_any" or
-           opt['training_mode'] == "uvwf_parallel" or
-           opt['training_mode'] == "uvwf_direction" or
-           opt['training_mode'] == "hhd"):
-            reconstructed = model_reconstruction(model, data, opt)
+            opt['training_mode'] == "dsfm_parallel" or
+            opt['training_mode'] == "dsfm_direction" or
+            opt['training_mode'] == "uvw" or 
+            opt['training_mode'] == "uvwf_any" or
+            opt['training_mode'] == "uvwf_parallel" or
+            opt['training_mode'] == "uvwf_direction" or
+            opt['training_mode'] == "hhd"):
+            model_reconstruction(model, data, opt)
         else:
             print(f"Training mode {opt['training_mode']} does not support the reconstruction task")
-
+    if("streamfunction" in tests):
+        if("dsf" in opt['training_mode'] or \
+            "uvwf" in opt['training_mode']):
+            model_stream_function(model, data, opt)
+        else:
+            print(f"Training mode {opt['training_mode']} does not support the stream function task")
+    
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Evaluate a model on some tests')
 
