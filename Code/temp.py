3c22e4af7cbbc0fab6024720b5c43bfc9770ea20
Skylar Wurster <swwurster@gmail.com>
2022-06-27 14:16:35 -0400

    Update temp.py

--- Code/temp.py
+++ Code/temp.py
@@ -4,7 +4,8 @@ import netCDF4
 from netCDF4 import Dataset
 import h5py
 import numpy as np
-
+import time
+from Other.utility_functions import nc_to_tensor
 
 project_folder_path = os.path.dirname(os.path.abspath(__file__))
 project_folder_path = os.path.join(project_folder_path, "..")
@@ -13,13 +14,23 @@ output_folder = os.path.join(project_folder_path, "Output")
 save_folder = os.path.join(project_folder_path, "SavedModels")
 
 if __name__ == '__main__':
-    test_file = "tornado.nc"
-    d = Dataset(os.path.join(data_folder, test_file))
+    test_file = "isotropic_1024^3.nc"
+    t = time.time()
+    data = Dataset(os.path.join(project_folder_path, test_file))
+    t_1 = time.time()
+    print(f"Took {t_1-t : 0.04f} sec. to load dataset as NetCDF")
+    t = time.time()
     channels = []
-    for a in d.variables:
-        n = np.array(d[a])
-        channels.append(n)
-    n = np.stack(channels)
-    n = torch.tensor(n).unsqueeze(0)
-    print(n.shape)
+    for a in data.variables:
+        d = np.array(data[a])
+        channels.append(d)
+    t_1 = time.time()
+    d = np.stack(channels)
+    print(d.shape)
+    print(f"Took {t_1-t : 0.04f} sec. to load dataset to RAM (numpy).")
+    t = time.time()
+    d = torch.tensor(d).unsqueeze(0)
+    t_1 = time.time()
+    print(d.shape)
+    print(f"Took {t_1-t : 0.04f} sec. to move numpy data to tensor format.")
     quit()
